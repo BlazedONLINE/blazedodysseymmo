086139cf4d87c446b634102b3de128156ec8bdb4
Daniel Murphy <110000353+BlazedPYT@users.noreply.github.com>
2025-09-09 01:36:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-window.BO_CONFIG = { API_BASE_URL: 'https://api.blessedtk.com' };
+window.BTK_CONFIG = {
+  discord_url: "https://discord.gg/5Dncrg2PNn",
+  github_url: "https://github.com/BlazedONLINE/blazedodysseymmo",
+  terms_url: "#",
+  privacy_url: "#",
+  trailer_url: "https://www.youtube.com/watch?v=RBE-C-b86-Y"
+};
