4b7cc331b7a4b9f066e02694f069551e7f27a7aa
Daniel Murphy <110000353+BlazedPYT@users.noreply.github.com>
2025-09-11 00:36:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// config.js
+// Global config (load this BEFORE app.js / account.js / etc.)
 window.BTK_CONFIG = {
   API_BASE_URL: "https://api.blessedtk.com",
   discord_url: "https://discord.gg/5Dncrg2PNn",
   github_url: "https://github.com/BlazedONLINE/blazedodysseymmo",
-  terms_url: "#",
-  privacy_url: "#",
-  // Use the EMBED id (the bit after v=)
-  trailer_embed_id: "RBE-C-b86-Y"
+  terms_url: "terms.html",
+  privacy_url: "privacy.html",
+  trailer_url: "https://www.youtube.com/watch?v=RBE-C-b86-Y"
 };
